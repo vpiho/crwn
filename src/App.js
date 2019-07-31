5d6014712e0ec77ee8b1326b7e7b0a8ba6da3a9c
Valentin Piho <vpiho.js@gmail.com>
2019-07-31 17:01:28 +0300

    Shop data updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { setCurrentUser } from './redux/user/user.actions'
 
-import './App.css'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import Header from './components/header/header.component.jsx'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
 import SignInUp from './pages/sing-in-up/sign-in-up.component.jsx'
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import { setCurrentUser } from './redux/user/user.actions'
+
+import './App.css'
 
 class App extends React.Component {
   unsubscribeFromAuth = null
